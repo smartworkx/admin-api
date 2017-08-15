735753d6d3284b6a7ef8a17378b9d2bf12ba83ea
Joris Wijlens <joris@smartworkx.nl>
2017-08-15 22:23:47 +0200

    Improved deploy script

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-PROJECT_NAME=admin-api
-IMAGE_TAG=$(jq '.kubernetes.deployments[] | select(.id==$PROJECT_NAME).version' ./staging/env.json)
-ZONE=europe-west1-c
-CLUSTER=smartworkx-cluster
-echo "deploy $IMAGE_TAG"
+project_name=admin-api
+image_tag=$(jq ".kubernetes.deployments[] | select(.id==$project_name).version" ./staging/env.json)
+zone=europe-west1-c
+cluster=smartworkx-cluster
+echo "deploy $image_tag"
 gcloud auth activate-service-account --key-file /tmp/service-account.json
 gcloud config set project smartworkx-173909
-gcloud container clusters get-credentials --zone $ZONE $CLUSTER
-sed s/IMAGE_TAG/$IMAGE_TAG/g; ./sources/kubernetes/kubernetes.tpl > ./deploy/kubernetes.yml
+gcloud container clusters get-credentials --zone $zone $cluster
+sed s/IMAGE_TAG/$image_tag/g; ./sources/kubernetes/kubernetes.tpl > ./deploy/kubernetes.yml
 echo "Create yaml in $(pwd) $(cat ./kubernetes.yml)"
 kubectl apply -f ./deploy/kubernetes.yml
