bf8e52eeaf696b4d0786d5397f34bad4f6ea2c69
Joris Wijlens <joris@smartworkx.nl>
2017-08-17 22:02:43 +0200

    Added ingress

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -4,6 +4,6 @@ echo "deploy $IMAGE_TAG"
 gcloud auth activate-service-account --key-file /tmp/service-account.json
 gcloud config set project smartworkx-173909
 gcloud container clusters get-credentials --zone europe-west1-c smartworkx-cluster
-sed s/IMAGE_TAG/$IMAGE_TAG/g ./sources/kubernetes/kubernetes.tpl > ./deploy/kubernetes.yml
+sed s/IMAGE_TAG/$IMAGE_TAG/g ./sources/kubernetes/kubernetes.template.yml > ./deploy/kubernetes.yml
 echo "Create yaml in $(pwd) $(cat ./kubernetes.yml)"
 kubectl apply -f ./deploy/kubernetes.yml
