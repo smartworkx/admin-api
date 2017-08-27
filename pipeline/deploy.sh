158b04ba1650d0caa58f598330f4f6e0cd58b827
Joris Wijlens <joris@smartworkx.nl>
2017-08-27 12:23:32 +0200

    Added slash to readiness

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -5,5 +5,5 @@ gcloud auth activate-service-account --key-file /tmp/service-account.json
 gcloud config set project smartworkx-173909
 gcloud container clusters get-credentials --zone europe-west1-c smartworkx-cluster
 sed s/IMAGE_TAG/$IMAGE_TAG/g ./sources/kubernetes/kubernetes.template.yml > ./deploy/kubernetes.yml
-echo "Create yaml in $(pwd) $(cat ./kubernetes.yml)"
+echo "Create yaml in $(pwd) $(cat ./deploy/kubernetes.yml)"
 kubectl apply -f ./deploy/kubernetes.yml
