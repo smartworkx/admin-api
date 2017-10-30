9aea08836e40196f6c5809047252d3f8f687de63
Joris Wijlens <joris@smartworkx.nl>
2017-10-30 23:27:34 +0100

    Vat 2017 q3

--- src/main/java/nl/smartworkx/admin/adapters/tax/vat/model/VatDeclarationFactory.java
+++ src/main/java/nl/smartworkx/admin/adapters/tax/vat/model/VatDeclarationFactory.java
@@ -2,6 +2,7 @@ package nl.smartworkx.admin.adapters.tax.vat.model;
 
 import static java.util.stream.Collectors.toSet;
 import static nl.smartworkx.admin.model.ledger.LedgerCode.DVAT;
+import static nl.smartworkx.admin.model.ledger.LedgerCode.TOJ;
 import static nl.smartworkx.admin.model.ledger.LedgerCode.VATS;
 
 import java.util.Set;
@@ -12,8 +13,8 @@ import nl.smartworkx.admin.model.Amount;
 import nl.smartworkx.admin.model.journal.JournalEntryCalculator;
 import nl.smartworkx.admin.model.journal.JournalEntryFinancialFact;
 import nl.smartworkx.admin.model.journal.JournalEntryRepository;
-import nl.smartworkx.admin.model.time.Quarter;
 import nl.smartworkx.admin.model.ledger.LedgerRepository;
+import nl.smartworkx.admin.model.time.Quarter;
 
 @Service
 public class VatDeclarationFactory {
@@ -35,8 +36,9 @@ public class VatDeclarationFactory {
 
         final Amount vatServiced = calculator.sum(VATS);
         final Amount deductedVat = calculator.sum(DVAT);
+        final Amount turnoverHigh = calculator.sum(TOJ);
         Set<VatJournalEntry> vatJournalEntries = calculator.getJournalEntryIds(DVAT, VATS).stream().map(VatJournalEntry::new).collect
                 (toSet());
-        return new VatDeclaration(quarter, vatServiced, deductedVat, vatJournalEntries);
+        return new VatDeclaration(quarter, vatServiced, deductedVat, turnoverHigh, vatJournalEntries);
     }
 }
--- src/main/java/nl/smartworkx/admin/model/ledger/LedgerCode.java
+++ src/main/java/nl/smartworkx/admin/model/ledger/LedgerCode.java
@@ -6,8 +6,10 @@ package nl.smartworkx.admin.model.ledger;
  * @since 1.0
  */
 public final class LedgerCode {
-	public final static String DVAT = "DVAT";
-	public final static String VATS = "VATS";
-	public static final String CRED = "CRED";
-	public static final String VATP = "VATP";
+    public final static String DVAT = "DVAT";
+    public final static String VATS = "VATS";
+    public static final String CRED = "CRED";
+    public static final String VATP = "VATP";
+    public static final String TOJ = "TOJ";
+
 }
